ec914df0d871db23a23f76eda03db6527397b3d9
Azat Gilyazov <azatpscl@gmail.com>
2021-04-08 17:45:39 +0300

    do refactor 2

--- beatsell/src/main/java/tech/anteeone/beatsell/controllers/admin/AdminController.java
+++ beatsell/src/main/java/tech/anteeone/beatsell/controllers/admin/AdminController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import tech.anteeone.beatsell.exceptions.UserNotFoundException;
-import tech.anteeone.beatsell.repositories.jpa.LicensesRepository;
 import tech.anteeone.beatsell.services.domain.interfaces.BeatsService;
 import tech.anteeone.beatsell.services.domain.interfaces.LicensesService;
 import tech.anteeone.beatsell.services.domain.interfaces.UserService;
--- beatsell/src/main/java/tech/anteeone/beatsell/services/domain/interfaces/UserService.java
+++ beatsell/src/main/java/tech/anteeone/beatsell/services/domain/interfaces/UserService.java
@@ -1,6 +1,6 @@
 package tech.anteeone.beatsell.services.domain.interfaces;
 
-import tech.anteeone.beatsell.exceptions.UserNotFoundException;
+import tech.anteeone.beatsell.utils.exceptions.UserNotFoundException;
 import tech.anteeone.beatsell.models.User;
 
 public interface UserService {
